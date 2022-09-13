909d030b7d2b35f33f09a2cad3b556e0a69b5783
MPilecarte <mayara.pilecarte@gmail.com>
2022-09-12 22:44:21 -0300

    Feature: new Controller 'PersonagensControllers'

---
+++ Controllers/PersonagensController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using RpgApi.Data;
+using System.Threading.Tasks;
+using RpgApi.Models;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RpgApi.Controllers
+{
+
+    [ApiController]
+    [Route("[Controller]")]
+    public class PersonagensController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public PersonagensController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            try
+            {
+                Personagem p = await _context.Personagens
+                    .FirstOrDefaultAsync(pBusca => pBusca.Id == id);
+
+                return Ok(p);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet ("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                LinkedList<Personagem> lista = await _context.Personagens.ToListAsync();
+                return Ok(lista);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
